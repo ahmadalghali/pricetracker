b9c552620110744fe90cc9a5258f4aece99e31d5
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2025-04-04 03:40:11 +0300

    initial commit

---
+++ src/test/java/uk/nhsbsa/pricetracker/PriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhsbsa.pricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
